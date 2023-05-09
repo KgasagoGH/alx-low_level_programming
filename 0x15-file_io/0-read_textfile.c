64a28353e6d7de180ad666a1ea8aaa210a7a09a6
KgasagoGH <kgasago.mash@gmail.com>
2023-05-09 22:12:00 +0200

    read file modified

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,65 @@
-#include "main.h"
-/**
-*  * read_textfile - reads a text file and prints it to the standard output
-*   * @filename: name of the file to be read
-*    * @letters: number of letters to read and print
-*     * Return: the number of letters printed, or 0 if it failed
-*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-int i, y;
-char *buf;
-if (!filename)
-return (0);
-fd = open(filename, O_RDONLY);
-if (fd < 0)
-return (0);
-buf = malloc(sizeof(char) * letters);
-if (!buf)
-return (0);
-i = read(fd, buf, letters);
-if (i < 0)
-{
-free(buf);
-return (0);
+    int fd;
+    ssize_t bytes_read, bytes_written;
+    char *buffer;
+
+    if (filename == NULL)
+        return 0;
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return 0;
+
+    buffer = malloc(letters);
+    if (buffer == NULL)
+    {
+        close(fd);
+        return 0;
+    }
+
+    bytes_read = read(fd, buffer, letters);
+    if (bytes_read == -1)
+    {
+        close(fd);
+        free(buffer);
+        return 0;
+    }
+
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    if (bytes_written == -1 || bytes_written != bytes_read)
+    {
+        close(fd);
+        free(buffer);
+        return 0;
+    }
+
+    close(fd);
+    free(buffer);
+
+    return bytes_written;
 }
-buf[i] = '\0';
-close(fd);
-y = write(STDOUT_FILENO, buf, i);
-if (y < 0)
+
+int main(int ac, char **av)
 {
-free(buf);
-return (0);
-}
-free(buf);
-return (y);
+    ssize_t n;
+
+    if (ac != 2)
+    {
+        dprintf(2, "Usage: %s filename\n", av[0]);
+        exit(1);
+    }
+
+    n = read_textfile(av[1], 114);
+    printf("\n(printed chars: %li)\n", n);
+
+    n = read_textfile(av[1], 1024);
+    printf("\n(printed chars: %li)\n", n);
+
+    return 0;
 }
